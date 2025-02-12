a6a86bf6294f3c179555b2bfb0d3479a6d58036f
Aziz <aziznono77@gmail.com>
2025-02-12 15:38:44 +0700

    new

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  {
+    ignores: ["node_modules", "dist", ".next"], // Ignore build and dependency folders
+  },
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  },
+  {
+    extends: "next/core-web-vitals",
+  },
+];
 
 export default eslintConfig;
