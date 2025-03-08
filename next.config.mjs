7eeddc2b68e13e7f222e4b8228652889ee0480ef
Aziz <aziznono77@gmail.com>
2025-03-08 16:10:43 +0700

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// eslint.config.mjs
+import js from '@eslint/js';
+import next from 'eslint-plugin-next';
+import prettier from 'eslint-plugin-prettier';
 
-export default nextConfig;
+export default [
+  js.configs.recommended,
+  next.configs.recommended,
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+    },
+  },
+];
